139cdd022c4affcf70cc4b6b371084e8f1d3c90e
Claudia de Jesus Dantas <claudiadejesusdantas@gmail.com>
2023-02-08 17:50:49 -0300

    entering characters from the keyboard

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let words = ["programacao","oracle", "alura","html", "javascript", "css", "logica"];
 let gameBoard = document.getElementById("gallows").getContext('2d');
 let secretWord = "";
+let userLetters = [];
 
 function chooseSecretWord(){
     let word = words[Math.floor(Math.random()*words.length)]
@@ -9,10 +10,43 @@ function chooseSecretWord(){
     console.log(secretWord)
 }
 
+function verifyWord(key){
+    let state = false;
+    if(key >= 65 && userLetters.indexOf(key) || key <= 90){
+        userLetters.push(key)
+        console.log(key)
+        console.log(userLetters)
+        return state
+    }
+    else{
+        state = true
+        userLetters.push(key)
+        console.log(key)
+        console.log(userLetters)
+        return state
+    }
+}
+
+
 function startGame(){
     document.getElementById('container_main').style.display = "none";
     chooseSecretWord();
+
     drawCanvas();
     drawLines();
+
+    document.onkeydown = (e) => {
+        let letra = e.key.toUpperCase()
+        if (verifyWord(letra)){
+
+        }
+    }
 }
+
+//captar a tecla digitada
+//verificar se é uma letra
+//saber se a palavra secreta inclui a letra digitada
+//escrever letra correta
+//escrever letra incorreta
+//contar os erros
 
